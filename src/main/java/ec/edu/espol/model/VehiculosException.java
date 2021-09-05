00f66801997b22cc7445f2559eebd7fca0e74c9d
ErickFloresBeltran <erfeflor@espol.edu.ec>
2021-09-04 23:50:32 -0500

    Se creó una clase error para la filtración de vehículos en el controller OfertarPorVehiculo

---
+++ src/main/java/ec/edu/espol/model/VehiculosException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+/**
+ *
+ * @author Erick
+ */
+public class VehiculosException extends RuntimeException{
+    public VehiculosException(String msj){
+        super(msj);
+    }
+}
